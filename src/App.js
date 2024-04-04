72dd5aac8a1c3090853ed5b5d3b7e0c4bc748e30
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-04-04 13:10:18 +0530

    layout done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components";
-import './App.css'
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
@@ -11,11 +18,46 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
+
       <div className="main">
-        
-      </div>
+        <Layout>
+          <div className="main">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+
+              <Route exact path="/crypto/:coinID">
+                <CryptoDetails />
+              </Route>
+
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       <div className="footer">
-        
+        <Typography.Title level={5} style={{color : 'white' , textAlign: 'center'}} >
+          Crytoverse
+          <br/>
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+
+      </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,27 @@ const Navbar = () => {
       <div className="logo-container">
         <Avatar src={icon} size='large' />
         <Typography.Title level={2} className='logo'>
-          <Link to="/"> Cryptohub</Link>
+          <Link to="/"> Cryptoverse</Link>
         </Typography.Title>
       </div>
 
-      <Menu>
+      <Menu theme='dark'>
+
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to='/'>Home</Link>
+        </Menu.Item>
+
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+        </Menu.Item>
+
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to='/exchanges'>Exchanges</Link>
+        </Menu.Item>
+
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to='/news'>News</Link>
+        </Menu.Item>
 
       </Menu>
 
